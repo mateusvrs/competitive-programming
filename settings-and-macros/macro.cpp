da96b197941673e5c5a3570689da13a7ae2abc58
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-04 02:29:12 -0300

    cpp: macros and settings

---
+++ settings-and-macros/macro.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vii;
+
+#define all(x) x.begin(), x.end()
+#define vin(vt) for (auto &e : vt) cin >> e
+#define LSOne(S) ((S) & -(S))
+#define MSOne(S) (1ull << (63 - __builtin_clzll(S)))
+
+const vii dir4{ {1,0},{-1,0},{0,1},{0,-1} };
+
+auto solve() {  }
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll t = 1;
+    //cin >> t;
+
+    while (t--) solve();
+
+    return 0;
+}
---
+++ settings-and-macros/short-macro.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+#define all(x) x.begin(), x.end()
+#define vin(vt) for (auto &e : vt) cin >> e
+
+auto solve() {  }
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll t = 1;
+    //cin >> t;
+
+    while (t--) solve();
+
+    return 0;
+}
