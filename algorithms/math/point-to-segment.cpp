ff6d7e3150973cfd7434a5641aae8c846c482dba
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-04 02:31:14 -0300

    algo: math pt2segment function

---
+++ algorithms/math/point-to-segment.cpp
@@ -0,0 +1,31 @@
+typedef pair<double, double> pdb;
+
+#define fst first
+#define snd second
+
+double pt2segment(pdb A, pdb B, pdb E) {
+  pdb AB = {B.fst - A.fst, B.snd - A.snd};
+  pdb BE = {E.fst - B.fst, E.snd - B.snd};
+  pdb AE = {E.fst - A.fst, E.snd - A.snd};
+
+  double AB_BE = AB.fst * BE.fst + AB.snd * BE.snd;
+  double AB_AE = AB.fst * AE.fst + AB.snd * AE.snd;
+
+  double ans;
+  if (AB_BE > 0) {
+    double y = E.snd - B.snd;
+    double x = E.fst - B.fst;
+    ans = sqrt(x * x + y * y);
+  } else if (AB_AE < 0) {
+    double y = E.snd - A.snd;
+    double x = E.fst - A.fst;
+    ans = sqrt(x * x + y * y);
+  } else {
+    auto [x1, y1] = AB;
+    auto [x2, y2] = AE;
+    double mod = sqrt(x1 * x1 + y1 * y1);
+    ans = abs(x1 * y2 - y1 * x2) / mod;
+  }
+
+  return ans;
+}
