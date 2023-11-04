3ef00ec77ca6ce6d3ac5d47f6def6445dc676bfa
Caio <caiofeliperro@gmail.com>
2023-11-04 18:28:49 -0300

    math(fast-exp): add fast exp algorithm

---
+++ algorithms/math/fast-exp-iterative.cpp
@@ -0,0 +1,15 @@
+long long fast_exp_it(long long a, int n)
+{
+    long long res = 1, base = a;
+
+    while (n)
+    {
+        if (n & 1)
+            res *= base;
+
+        base *= base;
+        n >>= 1;
+    }
+
+    return res;
+}
