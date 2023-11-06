2104eb6a71421b085f9d3af17f05272bc9f6f845
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-06 12:24:17 -0300

    algo(graphs): path with pred vect for dijkstra

---
+++ algorithms/graphs/dijkstra-path.cpp
@@ -0,0 +1,25 @@
+pair<vl, vl> Graph::dijkstra_path(ll src) {
+  vl pd(this->N, LLONG_MAX), ds(this->N, LLONG_MAX);
+  pd[src] = src;
+  ds[src] = 0;
+
+  set<pll> st;
+  st.emplace(0, src);
+
+  while (!st.empty()) {
+    ll u = st.begin()->snd;
+    ll wu = st.begin()->fst;
+    st.erase(st.begin());
+
+    if (wu != ds[u]) continue;
+    for (auto& [v, w] : adj[u]) {
+      if (ds[v] > ds[u] + w) {
+        ds[v] = ds[u] + w;
+        pd[v] = u;
+        st.emplace(ds[v], v);
+      }
+    }
+  }
+
+  return {ds, pd};
+}
