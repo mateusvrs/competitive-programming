5f50fd34173bc38060a53a6f770e1c59de3cc533
Caio <caiofeliperro@gmail.com>
2023-11-08 11:36:03 -0300

    graph(bellman-ford): add bellman-ford algorithms

---
+++ algorithms/graphs/bellman-ford-path.cpp
@@ -0,0 +1,19 @@
+using edge = tuple<int, int, int>;
+
+pair<vi, vi>
+bellman_ford(int s, int N, const vector<edge>& edges)
+{
+    vi dist(N + 1, oo), pred(N + 1, oo);
+    
+    dist[s] = 0;
+    pred[s] = s;
+
+    for (int i = 1; i <= N - 1; i++)
+        for (auto [u, v, w] : edges)
+            if (dist[u] < oo and dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w;
+                pred[v] = u;
+            }
+
+    return { dist, pred };
+}
