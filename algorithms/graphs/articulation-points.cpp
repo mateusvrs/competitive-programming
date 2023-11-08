6d0bfb202156e10f07613b90a13d9f1c5387da32
Caio <caiofeliperro@gmail.com>
2023-11-08 11:36:37 -0300

    graph(bridges): add bridges algorithms

---
+++ algorithms/graphs/articulation-points.cpp
@@ -0,0 +1,42 @@
+int dfs_num[MAX], dfs_low[MAX];
+vi adj[MAX];
+
+int dfs_articulation_points(int u, int p, int& next, set<int>& points)
+{
+    int children = 0;
+    dfs_low[u] = dfs_num[u] = next++;
+
+    for (auto v : adj[u])
+        if (not dfs_num[v]) {
+            ++children; 
+
+            dfs_articulation_points(v, u, next, points);
+
+            if (dfs_low[v] >= dfs_num[u])
+                points.insert(u);
+
+            dfs_low[u] = min(dfs_low[u], dfs_low[v]);
+        } else if (v != p)
+            dfs_low[u] = min(dfs_low[u], dfs_num[v]);
+
+    return children;
+}
+
+set<int> articulation_points(int N)
+{
+    memset(dfs_num, 0, (N + 1)*sizeof(int));
+    memset(dfs_low, 0, (N + 1)*sizeof(int));
+
+    set<int> points;
+
+    for (int u = 1, next = 1; u <= N; ++u)
+        if (not dfs_num[u])
+        {
+            auto children = dfs_articulation_points(u, u, next, points);
+
+            if (children == 1)
+                points.erase(u);
+        }
+    
+    return points;
+}
