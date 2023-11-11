479d48b3d8b4c649d14355a0e6d8eebcca937486
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-11 01:41:27 -0300

    algo(graphs): padronize dist algos w/ pred vectors

--- algorithms/graphs/bellman-ford.cpp
+++ algorithms/graphs/bellman-ford.cpp
@@ -1,12 +1,17 @@
 using edge = tuple<int, int, int>;
 
-vi bellman_ford(int s, int N, const vector<edge>& edges) {
-  vi dist(N + 1, oo);
+pair<vi, vi> bellman_ford(int s, int N, const vector<edge>& edges) {
+  vi dist(N + 1, oo), pred(N + 1, oo);
+
   dist[s] = 0;
+  pred[s] = s;
 
   for (int i = 1; i <= N - 1; i++)
     for (auto [u, v, w] : edges)
-      if (dist[u] < oo and dist[v] > dist[u] + w) dist[v] = dist[u] + w;
+      if (dist[u] < oo and dist[v] > dist[u] + w) {
+        dist[v] = dist[u] + w;
+        pred[v] = u;
+      }
 
-  return dist;
+  return {dist, pred};
 }
