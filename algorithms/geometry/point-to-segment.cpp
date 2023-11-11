dd9bf54aad1e89f88eb35df71a7f1db029a1fc1c
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-11 01:40:41 -0300

    algo(geometry): change from sqrt to hypot

--- algorithms/geometry/point-to-segment.cpp
+++ algorithms/geometry/point-to-segment.cpp
@@ -1,8 +1,5 @@
 typedef pair<double, double> pdb;
 
-#define fst first
-#define snd second
-
 double pt2segment(pdb A, pdb B, pdb E) {
   pdb AB = {B.fst - A.fst, B.snd - A.snd};
   pdb BE = {E.fst - B.fst, E.snd - B.snd};
@@ -15,15 +12,15 @@ double pt2segment(pdb A, pdb B, pdb E) {
   if (AB_BE > 0) {
     double y = E.snd - B.snd;
     double x = E.fst - B.fst;
-    ans = sqrt(x * x + y * y);
+    ans = hypot(x, y);
   } else if (AB_AE < 0) {
     double y = E.snd - A.snd;
     double x = E.fst - A.fst;
-    ans = sqrt(x * x + y * y);
+    ans = hypot(x, y);
   } else {
     auto [x1, y1] = AB;
     auto [x2, y2] = AE;
-    double mod = sqrt(x1 * x1 + y1 * y1);
+    double mod = hypot(x1, y1);
     ans = abs(x1 * y2 - y1 * x2) / mod;
   }
 
