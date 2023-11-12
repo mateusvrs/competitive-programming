1027ced6353ff7f5bcf695be0c565f00ff3c8b56
Caio <caiofeliperro@gmail.com>
2023-11-12 15:26:56 -0300

    algorithms(mst): add kruskal algorithm

---
+++ algorithms/graphs/kruskal.cpp
@@ -0,0 +1,20 @@
+using edge = tuple<int, int, int>;
+
+int kruskal(int N, vector<edge>& es)
+{
+    sort(es.begin(), es.end());
+
+    int cost = 0;
+    UnionFind ufds(N);
+
+    for (auto [w, u, v] : es)
+    {
+        if (not ufds.same_set(u, v))
+        {
+            cost += w;
+            ufds.union_set(u, v);
+        }
+    }
+
+    return cost;
+}
