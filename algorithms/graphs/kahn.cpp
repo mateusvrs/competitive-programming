cb3deccf2650fecd38fea6d2297a7ce200de7ad3
caio-felipee <caio-felipee@users.noreply.github.com>
2023-11-17 22:29:48 +0000

    clang(format): cpp files correct indentation

--- algorithms/graphs/kahn.cpp
+++ algorithms/graphs/kahn.cpp
@@ -1,29 +1,24 @@
 unordered_set<int> in[MAX], out[MAX];
 
-vi topological_sort(int N)
-{
-    vi o;
-    queue<int> q;
+vi topological_sort(int N) {
+  vi o;
+  queue<int> q;
 
-    for (int u = 1; u <= N; ++u)
-        if (in[u].empty())
-            q.push(u);
-    
-    while (not q.empty())
-    {
-        auto u = q.front();
-        q.pop();
+  for (int u = 1; u <= N; ++u)
+    if (in[u].empty()) q.push(u);
 
-        o.emplace_back(u);
+  while (not q.empty()) {
+    auto u = q.front();
+    q.pop();
 
-        for (auto v : out[u])
-        {
-            in[v].erase(u);
+    o.emplace_back(u);
 
-            if (in[v].empty())
-                q.push(v);
-        }
+    for (auto v : out[u]) {
+      in[v].erase(u);
+
+      if (in[v].empty()) q.push(v);
     }
+  }
 
-    return (int) o.size() == N ? o : vi { };
+  return (int)o.size() == N ? o : vi{};
 }
