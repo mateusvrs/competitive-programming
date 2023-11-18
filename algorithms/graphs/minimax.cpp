923bbde5979c09c898ddc4c7d71861b41f086f91
Caio <caiofeliperro@gmail.com>
2023-11-17 21:20:37 -0300

    algorithms(graph): add minimax algorithm

---
+++ algorithms/graphs/minimax.cpp
@@ -0,0 +1,32 @@
+vii adj[MAX];
+
+int minimax(int u, int N)
+{
+    set<int> C;
+    C.insert(u);
+
+    priority_queue<ii, vii, greater<ii>> pq;
+
+    for (auto [v, w] : adj[u])
+        pq.push(ii(w, v));
+
+    int minmax = -oo;
+
+    while ((int) C.size() < N)
+    {
+        int v, w;
+
+        do {
+            w = pq.top().first, v = pq.top().second;
+            pq.pop();
+        } while (C.count(v));
+
+        minmax = max(minmax, w);
+        C.insert(v);
+
+        for (auto [s, p] : adj[v])
+            pq.push(ii(p, s));
+    }
+
+    return minmax;
+}
