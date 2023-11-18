71a98963830824d04d42fdba57b8f662b2d6de1f
caio-felipee <caio-felipee@users.noreply.github.com>
2023-11-18 00:21:48 +0000

    clang(format): cpp files correct indentation

--- algorithms/graphs/minimax.cpp
+++ algorithms/graphs/minimax.cpp
@@ -1,32 +1,28 @@
 vii adj[MAX];
 
-int minimax(int u, int N)
-{
-    set<int> C;
-    C.insert(u);
+int minimax(int u, int N) {
+  set<int> C;
+  C.insert(u);
 
-    priority_queue<ii, vii, greater<ii>> pq;
+  priority_queue<ii, vii, greater<ii>> pq;
 
-    for (auto [v, w] : adj[u])
-        pq.push(ii(w, v));
+  for (auto [v, w] : adj[u]) pq.push(ii(w, v));
 
-    int minmax = -oo;
+  int minmax = -oo;
 
-    while ((int) C.size() < N)
-    {
-        int v, w;
+  while ((int)C.size() < N) {
+    int v, w;
 
-        do {
-            w = pq.top().first, v = pq.top().second;
-            pq.pop();
-        } while (C.count(v));
+    do {
+      w = pq.top().first, v = pq.top().second;
+      pq.pop();
+    } while (C.count(v));
 
-        minmax = max(minmax, w);
-        C.insert(v);
+    minmax = max(minmax, w);
+    C.insert(v);
 
-        for (auto [s, p] : adj[v])
-            pq.push(ii(p, s));
-    }
+    for (auto [s, p] : adj[v]) pq.push(ii(p, s));
+  }
 
-    return minmax;
+  return minmax;
 }
