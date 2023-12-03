937ff0ad75c924a85fcadcbbf209e7486cd4d0bc
mateuvrs <mateusvrs.dev@gmail.com>
2023-12-03 10:53:51 -0300

    algo(ds): add private in class to default vars

--- algorithms/data-structures/segtree.cpp
+++ algorithms/data-structures/segtree.cpp
@@ -1,10 +1,11 @@
 template <typename T, auto op>
 class SegTree {
- public:
+ private:
   T e;
   ll N;
   vector<T> seg;
 
+ public:
   SegTree(ll N, T e) : e(e), N(N), seg(N + N, e) {}
 
   void assign(ll i, T v) {
@@ -15,14 +16,12 @@ class SegTree {
 
   T query(ll l, ll r) {
     T la = e, ra = e;
-    l += N;
-    r += N;
+    l += N; r += N;
 
     while (l <= r) {
       if (l & 1) la = op(la, seg[l++]);
       if (~r & 1) ra = op(seg[r--], ra);
-      l >>= 1;
-      r >>= 1;
+      l >>= 1; r >>= 1;
     }
 
     return op(la, ra);
