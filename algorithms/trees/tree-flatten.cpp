150ed50c100be6f4f6a6b15ddbc13d682d73f18b
mateusvrs <mateusvrs.dev@gmail.com>
2024-05-13 12:18:02 -0300

    algo(trees): flatten to padronize tree

---
+++ algorithms/trees/tree-flatten.cpp
@@ -0,0 +1,18 @@
+vll tree_flatten(ll root) {
+    vl pre;
+    pre.reserve(N);
+
+    vll flat(N);
+    ll timer = -1;
+    auto dfs = [&](auto&& self, ll u, ll p) -> void {
+        timer++;
+        pre.push_back(u);
+        for (auto [v, w] : adj[u]) if (v != p) {
+            self(self, v, u);
+        }
+        flat[u].second = timer;
+        };
+    dfs(dfs, root, -1);
+    for (ll i = 0; i < (ll)N; i++) flat[pre[i]].first = i;
+    return flat;
+}
