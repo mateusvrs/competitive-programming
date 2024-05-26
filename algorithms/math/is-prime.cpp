1237b2acf2261a2934bdc1b502939a6201b1e8a5
mateusvrs <mateusvrs.dev@gmail.com>
2024-05-25 22:29:22 -0300

    algo(math): fix for loop break statement with <=

--- algorithms/math/is-prime.cpp
+++ algorithms/math/is-prime.cpp
@@ -2,7 +2,7 @@ bool isprime(ll n) {
   if (n < 2) return false;
   if (n == 2) return true;
   if (n % 2 == 0) return false;
-  for (ll i = 3; i * i < n; i += 2)
+  for (ll i = 3; i * i <= n; i += 2)
     if (n % i == 0) return false;
   return true;
 }
