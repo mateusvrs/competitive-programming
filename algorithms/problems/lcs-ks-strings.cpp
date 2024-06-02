c4d0a74270b8aa1a6a3257d556bbb2671c0b62e8
mateusvrs <mateusvrs.dev@gmail.com>
2024-06-02 14:38:56 -0300

    algo(problems): better lcs-ks with o(n) sf-a

--- algorithms/problems/lcs-ks-strings.cpp
+++ algorithms/problems/lcs-ks-strings.cpp
@@ -1,28 +1,27 @@
 int lcs_ks_strings(vector<string>& sts, int k) {
-  vi fml;
+  vector<int> fml;
   string t;
   for (int i = 0; i < k; i++) {
     t += sts[i];
     for (int j = 0; j < sts[i].size(); j++) fml.push_back(i);
   }
 
-  vi p = suffix_array(t);
-  deque<int> lcp = lcp_suffix_array(t, p);
-  lcp.push_front(0);
+  suffix_array sf(t);
+  sf.lcp.insert(sf.lcp.begin(), 0);
 
-  int l = 0, r = 0, cnt = 0, lcs = 0, n = p.size();
+  int l = 0, r = 0, cnt = 0, lcs = 0, n = sf.sa.size();
   vector<int> fr(k + 1);
   multiset<int> mst;
   while (l < n) {
     while (r < n and cnt < k) {
-      mst.insert(lcp[r]);
-      if (!fr[fml[p[r]]]++) cnt++;
+      mst.insert(sf.lcp[r]);
+      if (!fr[fml[sf.sa[r]]]++) cnt++;
       r++;
     }
-    mst.erase(mst.find(lcp[l]));
+    mst.erase(mst.find(sf.lcp[l]));
     if (mst.size() and cnt == k) lcs = max(lcs, *mst.begin());
-    fr[fml[p[l]]]--;
-    if (!fr[fml[p[l]]]) cnt--;
+    fr[fml[sf.sa[l]]]--;
+    if (!fr[fml[sf.sa[l]]]) cnt--;
     l++;
   }
 
