293b77b9ae353c9a8e29961fca28e1f4cee347bb
mateusvrs <mateusvrs.dev@gmail.com>
2024-06-06 01:25:28 -0300

    code(math): in-place fft cp algo

---
+++ algorithms/math/fft.cpp
@@ -0,0 +1,45 @@
+using cd = complex<double>;
+const double PI = acos(-1);
+
+void fft(vector<cd>& a, bool invert) {
+  int n = a.size();
+
+  for (int i = 1, j = 0; i < n; i++) {
+    int bit = n >> 1;
+    for (; j & bit; bit >>= 1)
+      j ^= bit;
+    j ^= bit;
+
+    if (i < j)
+      swap(a[i], a[j]);
+  }
+
+  for (int len = 2; len <= n; len <<= 1) {
+    double ang = 2 * PI / len * (invert ? -1 : 1);
+    cd wlen(cos(ang), sin(ang));
+    for (int i = 0; i < n; i += len) {
+      cd w(1);
+      for (int j = 0; j < len / 2; j++) {
+        cd u = a[i + j], v = a[i + j + len / 2] * w;
+        a[i + j] = u + v;
+        a[i + j + len / 2] = u - v;
+        w *= wlen;
+      }
+    }
+  }
+
+  if (invert) {
+    for (cd& x : a)
+      x /= n;
+  }
+}
+
+void fft_2d(vector<vector<cd>>& V, bool invert) {
+  for (int i = 0; i < V.size(); i++) fft(V[i], invert);
+  for (int i = 0; i < V.front().size(); i++) {
+    vector<cd> col(V.size());
+    for (int k = 0; k < V.size(); k++) col[k] = V[k][i];
+    fft(col, invert);
+    for (int k = 0; k < V.size(); k++) V[k][i] = col[k];
+  }
+}
