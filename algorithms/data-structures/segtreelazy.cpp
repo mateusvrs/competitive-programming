7c8af9e5b67e3f70b461c3fb38b0dc5b314d3214
mateusvrs <mateusvrs@users.noreply.github.com>
2024-06-22 03:29:20 +0000

    clang(format): cpp files correct indentation

--- algorithms/data-structures/segtreelazy.cpp
+++ algorithms/data-structures/segtreelazy.cpp
@@ -1,49 +1,49 @@
-template<typename T>
+template <typename T>
 class SegTreeLazy {
-private:
-    int N;
-    vector<T> seg, lzy;
+ private:
+  int N;
+  vector<T> seg, lzy;
 
-    void down(int k, int l, int r) {
-        seg[k] += (r - l + 1) * lzy[k];
-        if (l < r) {
-            lzy[k << 1] += lzy[k];
-            lzy[k << 1 | 1] += lzy[k];
-        }
-        lzy[k] = 0;
+  void down(int k, int l, int r) {
+    seg[k] += (r - l + 1) * lzy[k];
+    if (l < r) {
+      lzy[k << 1] += lzy[k];
+      lzy[k << 1 | 1] += lzy[k];
+    }
+    lzy[k] = 0;
+  }
+
+  void update(int i, int j, int k, int l, int r, T v) {
+    if (lzy[k]) down(k, l, r);
+    if (i > r or j < l) return;
+    if (i <= l and j >= r) {
+      seg[k] += (r - l + 1) * v;
+      if (l < r) {
+        lzy[k << 1] += v;
+        lzy[k << 1 | 1] += v;
+      }
+      return;
     }
 
-    void update(int i, int j, int k, int l, int r, T v) {
-        if (lzy[k]) down(k, l, r);
-        if (i > r or j < l) return;
-        if (i <= l and j >= r) {
-            seg[k] += (r - l + 1) * v;
-            if (l < r) {
-                lzy[k << 1] += v;
-                lzy[k << 1 | 1] += v;
-            }
-            return;
-        }
+    update(i, j, k << 1, l, (l + r) / 2, v);
+    update(i, j, k << 1 | 1, (l + r) / 2 + 1, r, v);
+    seg[k] = seg[k << 1] + seg[k << 1 | 1];
+  }
 
-        update(i, j, k << 1, l, (l + r) / 2, v);
-        update(i, j, k << 1 | 1, (l + r) / 2 + 1, r, v);
-        seg[k] = seg[k << 1] + seg[k << 1 | 1];
-    }
+  T query(int i, int j, int k, int l, int r) {
+    if (lzy[k]) down(k, l, r);
+    if (i > r or j < l) return 0;
+    if (i <= l and j >= r) return seg[k];
 
-    T query(int i, int j, int k, int l, int r) {
-        if (lzy[k]) down(k, l, r);
-        if (i > r or j < l) return 0;
-        if (i <= l and j >= r) return seg[k];
+    T lft = query(i, j, k << 1, l, (l + r) / 2);
+    T rgt = query(i, j, k << 1 | 1, (l + r) / 2 + 1, r);
+    return lft + rgt;
+  }
 
-        T lft = query(i, j, k << 1, l, (l + r) / 2);
-        T rgt = query(i, j, k << 1 | 1, (l + r) / 2 + 1, r);
-        return lft + rgt;
-    }
+ public:
+  SegTreeLazy(int N) : N(N), seg(N << 2, 0), lzy(N << 2, 0) {}
 
-public:
-    SegTreeLazy(int N) : N(N), seg(N << 2, 0), lzy(N << 2, 0) {}
+  void update(int i, int j, T v) { update(i, j, 1, 0, N - 1, v); }
 
-    void update(int i, int j, T v) { update(i, j, 1, 0, N - 1, v); }
-
-    T query(int i, int j) { return query(i, j, 1, 0, N - 1); }
+  T query(int i, int j) { return query(i, j, 1, 0, N - 1); }
 };
